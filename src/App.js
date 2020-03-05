e4a5ac44947dbdbd5b6bced8cc48d31e413624b0
muqsith <muqsithck@gmail.com>
2020-03-05 23:45:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import Button from "./components/button";
 function App() {
   return (
     <div className="flex flex-col w-3/4 mx-auto my-12 items-center">
-      <h1>React App with Tailwind css</h1>
+      <h1>React Js with Tailwind Css</h1>
       <Button onClick={() => console.log("I was clicked")}>
-       This is a button
+        <a href="https://tailwindcss.com/docs" target='_blank'> Tailwind Css </a>
+      </Button>
+      <Button onClick={() => console.log("I was clicked")}>
+        <a href="https://www.smashingmagazine.com/2020/02/tailwindcss-react-project/" target='_blank'>Setting Up</a>
       </Button>
     </div>
   );
--- src/components/button.js
+++ src/components/button.js
@@ -5,6 +5,7 @@ export default function Button({ children, ...buttonProps }) {
     <button
       className="px-5 py-2 m-2 rounded-lg bg-green-400 text-green-800 text-xl font-light uppercase shadow-md hover:shadow-lg"
       {...buttonProps}
+      
     >
       {children}
     </button>
